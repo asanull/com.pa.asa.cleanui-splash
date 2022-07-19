1077b275a65e157b2954e3f3d9e66a81924d82ca
asanull <asanullvoid@gmail.com>
2022-07-19 13:53:52 +0100

    initial version

--- ui/mods/com.pa.asa.cleanui/splash.js
+++ ui/mods/com.pa.asa.cleanui/splash.js
@@ -1,24 +1,53 @@
 var splash = {};
 splash.document = document.createElement("div");
 splash.parent = document.getElementById("content-wrapper").children[1];
-splash.interval =
-    setInterval(function () {
-        if (document.readyState == "complete") {
-            clearInterval(splash.interval);
-            splash.parent.appendChild(splash.document);
-        }
-    }, 100);
-
-splash.array = [
-    "Tried Bugs?",
-    "Documented!",
-    "That's no moon!",
-    "On steam!",
-    "Awesome community!",
-    "discord.gg/pa"
+splash.expansions = [
+    "Bug Faction",
+    "Legion Expansion",
+    "Second Wave",
+    "Section 17",
+    "Aurora-Artillery",
+    "Galactic War Overhaul",
+    "Queller AI",
+    "More Pew Pew"
 ]
-
-splash.text = splash.array[Math.floor(Math.random() * splash.array.length)].replace(/\s/g, "&nbsp;");
-
-splash.document.innerHTML = '<div id="splash">' + splash.text + '</div>';
+splash.othergames = [
+    "Total Annihilation",
+    "Beyond All Reason",
+    "BAR",
+    "Supreme Commander",
+    "SupCom"
+]
 splash.document.id = "tilt";
+splash.interval = setInterval(function () {
+    if (document.readyState == "complete") {
+        splash.player = document.getElementById("player-display-name").innerText;
+        splash.splashes = [
+            "Tried "+splash.expansions[Math.floor(Math.random()*splash.expansions.length)]+"?",
+            "Tried "+splash.othergames[Math.floor(Math.random()*splash.othergames.length)]+"?",
+            "Documented!",
+            "That's no moon!",
+            "On steam!",
+            "gg ez",
+            "Awesome community!",
+            "discord.gg/pa",
+            "100% more yellow text!",
+            "Quite Indie!",
+            "Endless!",
+            "Multithreaded!",
+            "Technically good!",
+            "Tell your friends!",
+            "Livestreamed!",
+            "Now with Titans!",
+            "Annihilate everything",
+            "Beware of Bugs! (The Faction)",
+            splash.player + " IS YOU",
+            "Hi Ben!",
+            "Planet to Planet!",
+            "Ferrets!"
+        ]
+        clearInterval(splash.interval);
+        splash.document.innerHTML = '<div id="splash">' + splash.splashes[Math.floor(Math.random() * splash.splashes.length)].replace(/\s/g, "&nbsp;") + '</div>';
+        splash.parent.appendChild(splash.document);
+    }
+}, 100);
